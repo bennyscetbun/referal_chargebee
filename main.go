b93961a3721271e88c605602bcd9412544ecad6d
Benny <scetbu.b@gmail.com>
2024-05-11 16:47:18 +0200

    add service

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/bennyscetbun/referal_chargebee/chargebee"
 	chargebeelib "github.com/chargebee/chargebee-go/v3"
@@ -12,8 +14,32 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func readArgumentsFromFile() []string {
+	// Check if there are enough arguments provided
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: program_name <file_path>")
+		os.Exit(1)
+	}
+
+	// Get the file path from command line arguments
+	filePath := os.Args[1]
+
+	// Read the entire file content
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	splitted := strings.Split((string)(data), "\n")
+	for i := range splitted {
+		splitted[i] = strings.TrimSpace(splitted[i])
+	}
+	return splitted
+}
+
 func main() {
-	chargebeelib.Configure(os.Args[2], os.Args[1])
+	myargs := readArgumentsFromFile()
+	chargebeelib.Configure(myargs[0], myargs[1])
 	r := gin.Default()
 	r.POST("/chargebee", chargebee.WebhookHandler)
 	tlsContext, tlsContextCancel := context.WithCancel(context.Background())
